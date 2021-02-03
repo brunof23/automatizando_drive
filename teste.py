b729373df2b58d0ee60eddb22ed0dec171eb001c
Programador <brunoferreira.tecnico@gmail.com>
2021-02-03 11:44:22 -0300

    Iniciando o projeto

--- teste.py
+++ teste.py
@@ -1,1 +1,9 @@
-import bokeh._testing.plugins.selenium
+from selenium import webdriver
+
+
+driver = webdriver.Chrome()
+
+driver.get('https://www.google.com.br/')
+driver.find_element_by_name('q').send_keys('Mercado Livre')
+driver.find_element_by_class_name('gNO89b').click()
+print('')
